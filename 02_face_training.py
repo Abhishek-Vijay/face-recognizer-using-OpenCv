88734ca23543cfb486e2db79f1ee6b3b82433bd4
Abhishek-Vijay <39898463+Abhishek-Vijay@users.noreply.github.com>
2018-08-18 15:08:05 +0530

    Update 02_face_training.py

--- 02_face_training.py
+++ 02_face_training.py
@@ -4,10 +4,6 @@ Training Multiple Faces stored on a DataBase:
 	==> LBPH computed model will be saved on trainer/ directory. (if it does not exist, pls create one)
 	==> for using PIL, install pillow library with "pip install pillow"
 
-Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
-
-Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18   
-
 '''
 
 import cv2
@@ -47,7 +43,7 @@ faces,ids = getImagesAndLabels(path)
 recognizer.train(faces, np.array(ids))
 
 # Save the model into trainer/trainer.yml
-recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
+recognizer.write('trainer/trainer.yml')
 
 # Print the numer of faces trained and end program
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
